055a8398f8ba285a7470fbbbbd63656057340c80
Santin04 <masantinte@gmail.com>
2024-10-26 18:20:57 -0300

    init study typescript

---
+++ basic_ts/index.js
@@ -0,0 +1,10 @@
+//npm i -g typescript => para instalar o typescript globalmente
+//tsc arquivo.ts, cria um arquivo js com o código do ts
+var numero01 = 10;
+var numero02 = 3;
+function exibir(n1, n2) {
+    var soma = n1 + n2;
+    console.log(soma);
+}
+;
+exibir(numero01, numero02);
---
+++ basic_ts/index.ts
@@ -0,0 +1,20 @@
+//npm i -g typescript => para instalar o typescript globalmente
+//tsc arquivo.ts => cria um arquivo js com o código do ts
+//tsc --init => cria o tsconfig
+
+//numbers
+const numero01: number = 10;
+const numero02: number = 3;
+
+function exibir(n1: number, n2: number) {
+    const soma: number = n1 + n2
+    console.log(soma);
+};
+
+exibir(numero01, numero02);
+
+//string
+let firstName: string = 'Mateus';
+
+//boolean
+let a: boolean = true;
