cbe04e345e9c99fe5b90c6b08ac73cd4110cb89d
Santin04 <masantinte@gmail.com>
2024-10-28 17:41:13 -0300

    functions

--- basic_ts/index.ts
+++ basic_ts/index.ts
@@ -1,14 +1,15 @@
 //npm i -g typescript => para instalar o typescript globalmente
 //tsc arquivo.ts => cria um arquivo js com o código do ts
 //tsc --init => cria o tsconfig
+//tsc -w => assim você não precisa ficar compilando toda hora o seu arquivo ts
 
 //numbers
 const numero01: number = 10;
 const numero02: number = 3;
 
-function exibir(n1: number, n2: number) {
-    const soma: number = n1 + n2
-    console.log(soma);
+function exibir(n1: number, n2: number): number {
+    const soma: number = n1 + n2;
+    return soma;
 };
 
 exibir(numero01, numero02);
@@ -18,3 +19,10 @@ let firstName: string = 'Mateus';
 
 //boolean
 let a: boolean = true;
+
+//array
+const numbers: number[] = [1, 2, 3];
+
+const nomes: string[] = ['Mateus', 'Santin'];
+
+//any => definir um dado como any, significa que ele aceita qualquer tipo (evitar)
