bf9d36299d171e8b023e43b0023e222f5af821d1
Santin04 <masantinte@gmail.com>
2024-11-05 22:11:37 -0300

    progress in objects of ts

---
+++ basic_ts/object.js
@@ -0,0 +1,41 @@
+function showDetails(product) {
+    console.log("O produto ".concat(product.name, " custa R$").concat(product.price, " "));
+    if (product.isAvaliable) {
+        console.log('O produto está disponivel');
+    }
+}
+var shirt = {
+    name: 'Camisa',
+    price: 19.99,
+    isAvaliable: true,
+};
+function showUser(user) {
+    console.log('O email do usuário é: ' + user.email);
+    if (user.role) {
+        console.log('E sua função é: ' + user.role);
+    }
+}
+var u1 = { email: 'mateus@gmail.com', role: 'admin' };
+var u2 = { email: 'roberto@gmail.com' };
+showUser(u1);
+showUser(u2);
+var fusca = {
+    brand: 'VW',
+    wheels: 4
+};
+var coords = {
+    x: 1
+};
+coords.y = 15;
+console.log(coords);
+var matheus = {
+    name: 'Mateus',
+    age: 30
+};
+console.log(matheus);
+var goku = {
+    name: 'Goku',
+    age: 512,
+    superPowers: ['kamehameha', 'genki dama']
+};
+console.log(goku);
--- basic_ts/object.ts
+++ basic_ts/object.ts
@@ -16,3 +16,70 @@ const shirt:Product = {
     price: 19.99,
     isAvaliable: true,
 }
+
+//propriedade opcional
+interface User {
+    email: string,
+    role?: string
+}
+
+function showUser(user: User) {
+    console.log('O email do usuário é: ' + user.email)
+
+    if (user.role) {
+        console.log('E sua função é: ' + user.role)
+    }
+}
+
+const u1: User = {email: 'mateus@gmail.com', role: 'admin'}
+const u2: User = {email: 'roberto@gmail.com'}
+
+showUser(u1)
+showUser(u2)
+
+//propriedade que não pode ser alterada (readonly)
+interface Car {
+    brand: string,
+    readonly wheels: number
+}
+
+const fusca: Car = {
+    brand: 'VW',
+    wheels: 4
+}
+
+//esse comando a baixo vai dar erro, pois está tentando atribuir outra valor em um readonly
+// fusca.wheels = 6
+
+interface Coords {
+    //todas as chaves tem que ser string e os valores numeros
+    [index: string]: number
+}
+
+let coords: Coords = {
+    x: 1
+}
+coords.y = 15
+console.log(coords)
+
+//extends
+interface Human {
+    name: string,
+    age: number
+}
+
+interface SuperHuman extends Human {
+    superPowers: string[]
+}
+
+const matheus: Human = {
+    name: 'Mateus',
+    age: 30
+}
+console.log(matheus)
+const goku: SuperHuman = {
+    name: 'Goku',
+    age: 512,
+    superPowers: ['kamehameha', 'genki dama']
+}
+console.log(goku)
